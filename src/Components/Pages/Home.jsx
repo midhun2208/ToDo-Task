742800e74efff5c657884ff168305e3419f56d69
midhun2208 <midhun.sm343@gmail.com>
2024-05-03 10:39:42 +0530

    Midhun P M

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -7,7 +7,7 @@ import Swal from "sweetalert2";
 
 const Home = () => {
   const list = useSelector((state) => state.toDo.items);
-
+  console.log(list);
 
   const [add, setAdd] = useState({
     id: list.length + 1,
@@ -28,7 +28,7 @@ const Home = () => {
     } else {
       dispatch(addItem(add));
       setAdd({
-        id: list.length + 1,
+        id: list.length +2 ,
         task: "",
         description: "",
         completed: false,
